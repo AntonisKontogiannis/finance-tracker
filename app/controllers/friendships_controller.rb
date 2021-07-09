8adc5ee7e7f547cc6fe1ed8940586944e0839738
Antonis Kontogiannis <antonios.kontogiannis94@gmail.com>
2021-07-09 21:32:07 +0300

    Add friendship relation, search, add, delete and show friends functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,22 @@
 class FriendshipsController < ApplicationController
     def index
     end
+
+    def create
+        friend = User.find(params[:friend])
+        friendship = Friendship.create(user_id: current_user.id, friend_id: friend.id)
+        if friendship.save
+            flash[:notice] = "Start following user"
+        else
+            flash[:alert] = "Something went wrong"
+        end
+        redirect_to friendships_path
+    end
+
+    def destroy
+        friendship = Friendship.find_by(user_id: current_user.id, friend_id: params[:id] )
+        friendship.destroy
+        flash[:notice] = "Stop following"
+        redirect_to friendships_path
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,32 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def search
+    if params[:user].present?
+      if User.where(first_name: params[:user]).any?
+        @friend1 = User.where(first_name: params[:user])
+      end
+      if User.where(last_name: params[:user]).any?
+        @friend1 = User.where(last_name: params[:user])
+      end
+      if User.where(email: params[:user]).any?
+        @friend1 = User.where(email: params[:user])
+      end
+      if !@friend1
+        flash[:alert] = "Couldn't find any user"
+        redirect_to friendships_path
+      end       
+    else
+      flash[:alert] = "Please enter an attribute to search"
+      redirect_to friendships_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
 end
+
+
